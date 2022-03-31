0bf4f5e4fcc0d3978b35fb6f7b38f7541ceb38f1
MochilaCheia <paulovvieirag@gmail.com>
2022-03-30 23:31:32 -0300

    Função linear dos itens

--- Jogos/sorteador.py
+++ Jogos/sorteador.py
@@ -1,60 +1,58 @@
 import random
 from time import sleep
 
-def sorteador_desenhos():
-
-    possiveis_desenhos = ['1- A rolling pin', '2- A hammock', '3- An anchor', '4- A skunk', '5- A desk chair',
-                          '6- Van Gogh’s ear', '7- A sandwich', '8- A string quartet', '9- An ottoman',
-                          '10- A bottle opener', '11- A fire escape', '12- Luminescent plankton', '13- A cabin',
-                          '14- Mushrooms', '15- Shrubbery', '16- Bob Marley', '17- A full house', '18- Bubbles'
-                          '19- Fangs', '20- A pickle', '21- An ironing board', '22- A dolphin', '23- A paper clip',
-                          '24- A trumpet', '25- A burrito', '26- Tube socks', '27- A crayon', '28- A robot',
-                          '29- A roller coaster', '30- A mosquito', '31- Fruit cocktail', '32- A peg leg',
-                          '33- A spigot', '34- A balloon', '35- Head in the clouds', '36- The Milky Way',
-                          '37- A knot', '38- An old key', '39- A submarine', '40- A tulip', '41- A gold medal',
-                          '42- Synchronized swimmers', '43- Pom-poms', '44- A juice box', '45- A jar full of pennies',
-                          '46- A bag of hammers', '47- Jelly beans', '48- A sundial', '49- A crystal ball',
-                          '50- A synthesizer', '51- A paper airplane', '52- A water tower', '53- A surfboard',
-                          '54- New Jersey', '55- Fiji', '56- A layer cake', '57- A panda', '58- A mime',
-                          '59- Charlie Chaplin', '60- A penguin', '61- A seagull', '62- David Bowie',
-                          '63- A sensitive cowboy', '64- A leopard', '65- An anatomy chart', '66- A tree limb',
-                          '67- A ship in a bottle', '68- A mouth', '69- Brass knuckles', '70- An ear of corn',
-                          '71- Costume jewelry', '72- A mirage', '73- Smoke', '74- Mold', '75- A rainbow',
-                          '76- A dollar bill', '77- A bone',
-                          '78- A glass of milk', '79- A teapot', '80- Weeds', '81- Dance steps',
-                          '82- A turkey leg', '83- A pencil',
-                          '84- A picket fence', '85- A Tiffany lamp', '86- The Empire State Building',
-                          '87- A stalagmite', '88- A stalactite', '89- A kiss', '90- A ladybug', '91- A helmet',
-                          '92- A paw print', '93- A Martian',
-                          '94- A T-shirt', '95- A cinder block', '96- Swim fins', '97- A ripe banana',
-                          '98- A barbell', '99- A tennis racket', '100- Japan', '101- A spiral staircase',
-                          '102- A ponytail', '103- A campfire', '104- A squirrel', '105- A thumb', '106- A book',
-                          '107- Girlish laughter', '108- Tangled ribbons', '109- Noodles', '110- Best friends',
-                          '111- Worst enemies', '112- A lock', '113- An accordion', '114- A log',
-                          '115- A melting candle', '116- A phone booth', '117- A geode', '118- Dice',
-                          '119- Ointment', '120- A bucket', '121- A digital watch', '122- A bicycle',
-                          '123- A cassette tape', '124- A library card', '125- A corn dog with mustard',
-                          '126- Mittens', '127- A pocket', '128- A bunch of grapes', '129- A vending machine',
-                          '130- A typewriter', '131- A flamingo', '132- A kebab', '133- Shelves', '134- A necklace',
-                          '135- A dirty rag', '136- A scallion pancake', '137- A time machine',
-                          '138- A Tyrannosaurus Rex', '139- A music box',
-                          '140- A candelabra', '141- A quarter', '142- A bulldog', '143- A fairy',
-                          '144- A ball of yarn', '145- A haircut', '146- An electric guitar', '147- Confetti',
-                          '148- A pair of scissors', '149- A bandage', '150- A watermelon', '151- Bacon',
-                          '152- A newsboy cap', '153- A seed pod', '154- A board game', '155- Daffodils',
-                          '156- An onion', '157- A slinky', '158- A keychain', '159- A sand castle',
-                          '160- Leftovers', '161- A ghost', '162- A vampire', '163- A football',
-                          '164- A lightbulb', '165- A horseshoe', '166- A pug', '167- The Sunday paper',
-                          '168- A helicopter', '169- A warthog', '170- A basket', '171- A skateboard',
-                          '172- Sea spray', '173- Seaweed', '174- Hills and valleys', '175- An umbrella',
-                          '176- A Christmas tree', '177- A hamburger', '178- Sunglasses', '179- A seal',
-                          '180- Twenty thousand leagues under the sea', '181- A shooting star', '182- Handprints',
-                          '183- A rotary phone', '184- A top hat', '185- A turtle', '186- A baseball', '187- Flags',
-                          '188- An airplane', '189- A microphone', '190- A county fair', '191- A saxophone',
-                          '192- An ice cream cone', '193- A marble', '194- A weathervane', '195- A lunchbox',
-                          '196- A pound', '197- A trolley car', '198- Cat whiskers', '199- A cleaver',
-                          '200- Electricity', '201- A Quonset hut', '202- A treasure chest', '203- Binoculars',
-                          '204- A pumpkin', '205- A chalkboard',
+possiveis_desenhos = ['1- A rolling pin', '2- A hammock', '3- An anchor', '4- A skunk', '5- A desk chair',
+                      '6- Van Gogh’s ear', '7- A sandwich', '8- A string quartet', '9- An ottoman',
+                      '10- A bottle opener', '11- A fire escape', '12- Luminescent plankton', '13- A cabin',
+                      '14- Mushrooms', '15- Shrubbery', '16- Bob Marley', '17- A full house', '18- Bubbles'
+                      '19- Fangs', '20- A pickle', '21- An ironing board', '22- A dolphin', '23- A paper clip',
+                      '24- A trumpet', '25- A burrito', '26- Tube socks', '27- A crayon', '28- A robot',
+                      '29- A roller coaster', '30- A mosquito', '31- Fruit cocktail', '32- A peg leg',
+                      '33- A spigot', '34- A balloon', '35- Head in the clouds', '36- The Milky Way',
+                      '37- A knot', '38- An old key', '39- A submarine', '40- A tulip', '41- A gold medal',
+                      '42- Synchronized swimmers', '43- Pom-poms', '44- A juice box', '45- A jar full of pennies',
+                      '46- A bag of hammers', '47- Jelly beans', '48- A sundial', '49- A crystal ball',
+                      '50- A synthesizer', '51- A paper airplane', '52- A water tower', '53- A surfboard',
+                      '54- New Jersey', '55- Fiji', '56- A layer cake', '57- A panda', '58- A mime',
+                      '59- Charlie Chaplin', '60- A penguin', '61- A seagull', '62- David Bowie',
+                      '63- A sensitive cowboy', '64- A leopard', '65- An anatomy chart', '66- A tree limb',
+                      '67- A ship in a bottle', '68- A mouth', '69- Brass knuckles', '70- An ear of corn',
+                      '71- Costume jewelry', '72- A mirage', '73- Smoke', '74- Mold', '75- A rainbow',
+                      '76- A dollar bill', '77- A bone',
+                      '78- A glass of milk', '79- A teapot', '80- Weeds', '81- Dance steps',
+                      '82- A turkey leg', '83- A pencil',
+                      '84- A picket fence', '85- A Tiffany lamp', '86- The Empire State Building',
+                      '87- A stalagmite', '88- A stalactite', '89- A kiss', '90- A ladybug', '91- A helmet',
+                      '92- A paw print', '93- A Martian',
+                      '94- A T-shirt', '95- A cinder block', '96- Swim fins', '97- A ripe banana',
+                      '98- A barbell', '99- A tennis racket', '100- Japan', '101- A spiral staircase',
+                      '102- A ponytail', '103- A campfire', '104- A squirrel', '105- A thumb', '106- A book',
+                      '107- Girlish laughter', '108- Tangled ribbons', '109- Noodles', '110- Best friends',
+                      '111- Worst enemies', '112- A lock', '113- An accordion', '114- A log',
+                      '115- A melting candle', '116- A phone booth', '117- A geode', '118- Dice',
+                      '119- Ointment', '120- A bucket', '121- A digital watch', '122- A bicycle',
+                      '123- A cassette tape', '124- A library card', '125- A corn dog with mustard',
+                      '126- Mittens', '127- A pocket', '128- A bunch of grapes', '129- A vending machine',
+                      '130- A typewriter', '131- A flamingo', '132- A kebab', '133- Shelves', '134- A necklace',
+                      '135- A dirty rag', '136- A scallion pancake', '137- A time machine',
+                      '138- A Tyrannosaurus Rex', '139- A music box',
+                      '140- A candelabra', '141- A quarter', '142- A bulldog', '143- A fairy',
+                      '144- A ball of yarn', '145- A haircut', '146- An electric guitar', '147- Confetti',
+                      '148- A pair of scissors', '149- A bandage', '150- A watermelon', '151- Bacon',
+                      '152- A newsboy cap', '153- A seed pod', '154- A board game', '155- Daffodils',
+                      '156- An onion', '157- A slinky', '158- A keychain', '159- A sand castle',
+                      '160- Leftovers', '161- A ghost', '162- A vampire', '163- A football',
+                      '164- A lightbulb', '165- A horseshoe', '166- A pug', '167- The Sunday paper',
+                      '168- A helicopter', '169- A warthog', '170- A basket', '171- A skateboard',
+                      '172- Sea spray', '173- Seaweed', '174- Hills and valleys', '175- An umbrella',
+                      '176- A Christmas tree', '177- A hamburger', '178- Sunglasses', '179- A seal',
+                      '180- Twenty thousand leagues under the sea', '181- A shooting star', '182- Handprints',
+                      '183- A rotary phone', '184- A top hat', '185- A turtle', '186- A baseball', '187- Flags',
+                      '188- An airplane', '189- A microphone', '190- A county fair', '191- A saxophone',
+                      '192- An ice cream cone', '193- A marble', '194- A weathervane', '195- A lunchbox',
+                      '196- A pound', '197- A trolley car', '198- Cat whiskers', '199- A cleaver',
+                      '200- Electricity', '201- A Quonset hut', '202- A treasure chest', '203- Binoculars',
+                      '204- A pumpkin', '205- A chalkboard',
 'Stiletto heels',
 'A crowd',
 'A pile of tires',
@@ -443,38 +441,57 @@ def sorteador_desenhos():
 'An ink pot',
 'A sippy cup',
 'Maple syrup',
-                          'A video game', 'Tectonic plates', 'A beach',
-                          'A wedding dress', 'A spelunker', 'A calculator', 'A baby monster', 'A transportation system',
-                          'A swamp', 'An invitation', 'An oven', 'A train', 'The Bermuda Triangle', 'A heart', 'A movie star',
-                          'A spiderweb', 'An igloo', 'Presidential pets', 'Paisley', 'A grandma', 'Lightning',
-                          'Wind', 'Run-D.M.C.', 'A tuxedo', 'A mayonnaise jar', 'A lemon meringue pie',
-                          'A sea urchin', 'A canyon', 'A cave', 'A concert', 'A viper', 'A phonograph', 'A bow', 'A convertible',
-                          'Ski slopes', 'A mummy', 'Broken glass', 'A bed', 'A bar of music', 'Polka dots', 'Woodgrain',
-                          'Plaid', 'Zigzag', 'A tacky rug', 'A plastic bag', 'A muffin tin', 'A sweater', 'A tuba', 'Yourself']
+                      'A video game', 'Tectonic plates', 'A beach',
+                      'A wedding dress', 'A spelunker', 'A calculator', 'A baby monster', 'A transportation system',
+                      'A swamp', 'An invitation', 'An oven', 'A train', 'The Bermuda Triangle', 'A heart', 'A movie star',
+                      'A spiderweb', 'An igloo', 'Presidential pets', 'Paisley', 'A grandma', 'Lightning',
+                      'Wind', 'Run-D.M.C.', 'A tuxedo', 'A mayonnaise jar', 'A lemon meringue pie',
+                      'A sea urchin', 'A canyon', 'A cave', 'A concert', 'A viper', 'A phonograph', 'A bow', 'A convertible',
+                      'Ski slopes', 'A mummy', 'Broken glass', 'A bed', 'A bar of music', 'Polka dots', 'Woodgrain',
+                      'Plaid', 'Zigzag', 'A tacky rug', 'A plastic bag', 'A muffin tin', 'A sweater', 'A tuba', 'Yourself']
+
+
+def sorteio_aleatorio():
 
     print('Olá, eu sou sou Sorte! seu sorteador de desenhos!\n~Seja bem vindo!~')
+
     sleep(1)
     print('>> Para utilizar o sistema, basta responder a pergunta do jeito que preferir'
           ' e para encerrar basta responder não a pergunta.<<')
+
     sleep(1)
-    print('Um arquivo de texto será criado para anotar quais desenhos você sortear, assim você desenhará o mesmo por engano.\n')
+    print('Um arquivo de texto será criado para anotar quais desenhos você sortear, '
+          'assim você não desenhará o mesmo por engano.\n')
 
     sorteio = input('Deseja sortear seu desenho?\n')
 
     while sorteio != 'não':
         numeros_sortidos = random.choice(possiveis_desenhos)
-        numero_itens = len(possiveis_desenhos)
         sleep(1)
         print(f'Você sorteou: {numeros_sortidos}. Divirta-se desenhando!\n')
+
+        numero_itens = len(possiveis_desenhos)
         sleep(1)
         print(f'E o número de opções restantes é: {numero_itens}\n')
+
         possiveis_desenhos.remove(numeros_sortidos)
+
         sleep(1)
         sorteio = input('Deseja continuar sorteando?\n')
 
 
+def sorterio_sequencia():
+
+    proxima = input('Deseja começar?\n')
+
+    for corrida in possiveis_desenhos:
+        confirmacao  = input('Deseja ver a sugestão de desenho?')
+        print(corrida)
+
+
 if __name__=='__main__':
-    print(sorteador_desenhos())
+    #print(sorteio_aleatorio())
+    print(sorterio_sequencia())
 
 
  # No arquivo de texto que for gerado pra adicionar os desenhos sorteados,
